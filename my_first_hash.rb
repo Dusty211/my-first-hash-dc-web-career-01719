63899ebe7b320cd1b7756da6110ccfa6df88fdd4
Kyle Houghton <kylebenz@hotmail.com>
2018-12-29 00:11:38 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+hash = {"glass_a" => "half_empty", "glass_b" => "half_full"}
   
 end
 
